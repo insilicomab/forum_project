cfe21779063525213de849ceba49592ed1aaf2da
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-10-10 13:18:30 +0900

    add script

--- boards/forms.py
+++ boards/forms.py
@@ -8,3 +8,18 @@ class CreateThemeForm(forms.ModelForm):
     class Meta:
         model = Themes
         fields = ('title',)
+
+
+class DeleteThemeForm(forms.ModelForm):
+
+    class Meta:
+        model = Themes
+        fields = []
+
+
+class PostCommentForm(forms.ModelForm):
+    comment = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 5, 'cols': 60}))
+
+    class Meta:
+        model = Comments
+        fields = ('comment', )
--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from . import forms
 from django.contrib import messages
 from .models import Themes
+from django.http import Http404
 
 
 '''
@@ -31,5 +32,69 @@ def list_themes(request):
     return render(
         request, 'boards/list_themes.html', context={
             'themes': themes
+        }
+    )
+
+'''
+投稿編集
+'''
+
+def edit_theme(request, id):
+    theme = get_object_or_404(Themes, id=id)
+    if theme.user.id != request.user.id:
+        raise Http404
+    edit_theme_form = forms.CreateThemeForm(request.POST or None, instance=theme)
+    if edit_theme_form.is_valid():
+        edit_theme_form.save()
+        messages.success(request, '掲示板を更新しました')
+        return redirect('boards:list_themes')
+    return render(
+        request, 'boards/edit_theme.html', context={
+            'edit_theme_form': edit_theme_form,
+            'id': id,
+        }
+    )
+
+'''
+投稿削除
+'''
+
+def delete_theme(request, id):
+    theme = get_object_or_404(Themes, id=id)
+    if theme.user.id != request.user.id:
+        raise Http404
+    delete_theme_form = forms.DeleteThemeForm(request.POST or None)
+    if delete_theme_form.is_valid(): # csrf check
+        theme.delete()
+        messages.success(request, '掲示板を削除しました')
+        return redirect('boards:list_themes')
+    return render(
+        request, 'boards/delete_theme.html', context={
+            'delete_theme_form': delete_theme_form,
+        }
+    )
+
+'''
+コメント機能
+'''
+
+def post_comments(request, theme_id):
+    saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
+    post_comment_form = forms.PostCommentForm(request.POST or None, initial={'comment': saved_comment})
+    theme = get_object_or_404(Themes, id=theme_id)
+    comments = Comments.objects.fetch_by_theme_id(theme_id)
+    if post_comment_form.is_valid():
+        if not request.user.is_authenticated:
+            raise Http404
+        post_comment_form.instance.theme = theme
+        post_comment_form.instance.user = request.user
+        post_comment_form.save()
+        cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
+        return redirect('boards:post_comments', theme_id=theme_id)
+    return render(
+        request, 'boards/post_comments.html', context={
+            'post_comment_form': post_comment_form,
+            'theme': theme,
+            'comments': comments,
         }
     )
